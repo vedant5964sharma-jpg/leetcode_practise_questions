80c8633d39f5e0d9f2a0948890bebaea1756671b
vedant5964sharma-jpg <vedant5964sharma@gmail.com>
2026-03-10 20:37:19 +0530

    Time: 0 ms (100%), Space: 11.5 MB (78.13%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    void sortColors(vector<int>& arr) {
+
+        int n=arr.size();
+        int low=0;int mid=0;int high=n-1;
+        while(mid<=high){
+            if(arr[mid]==0){
+                swap(arr[mid],arr[low]);
+                mid++;low++;
+                
+            }
+            else if(arr[mid]==1){
+                mid++;
+                
+            }
+            else{
+                swap(arr[high],arr[mid]);
+                high--;
+                
+            }
+        }
+        
+    }
+};
