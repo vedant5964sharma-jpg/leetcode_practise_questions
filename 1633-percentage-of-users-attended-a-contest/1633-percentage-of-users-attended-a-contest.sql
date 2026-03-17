86bee7af2d1c6d65e10d8f2027e2ebaf89537193
vedant5964sharma-jpg <vedant5964sharma@gmail.com>
2026-03-17 10:09:54 +0530

    Time: 848 ms (95.16%), Space: 0B (100%) - LeetHub

---
+++ 1633-percentage-of-users-attended-a-contest/1633-percentage-of-users-attended-a-contest.sql
@@ -0,0 +1,9 @@
+SELECT 
+    contest_id,
+    ROUND(
+        COUNT(r.user_id) * 100.0 / (SELECT COUNT(*) FROM Users),
+        2
+    ) AS percentage
+FROM Register r
+GROUP BY contest_id
+ORDER BY percentage DESC, contest_id;
