d49e5eca414be84e46c92048a4decf57e56356b6
vedant5964sharma-jpg <vedant5964sharma@gmail.com>
2026-09-25 13:59:42 +0530

    Time: 19 ms (46.43%), Space: 12.7 MB (76.79%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def isPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        i=0
+        j=len(s)-1
+        while i<j:
+            while i<j and not s[i].isalnum():
+                i+=1
+                continue
+            while i<j and not s[j].isalnum():
+                j-=1
+                continue
+            if s[i].lower()!=s[j].lower():
+                return False
+            i+=1
+            j-=1
+        return True            
